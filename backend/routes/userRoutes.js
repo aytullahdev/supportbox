4c6bf63ef640af66667aa40bad05c6ae8daa8e7d
unknown <ayt.ullah.dev@gmail.com>
2022-07-04 22:38:08 +0600

    Mongodb connected with mongoose

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {registerUser,loginUser} = require('../controllers/userController');
 
+
 //Routes
 router.post('/',registerUser)
 router.post('/login',loginUser)
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
+
 const {errorHandler} = require('./middleware/errorMiddleware.js')
+const connectDB = require('./config/db')
 
-
+connectDB();
 const app = express();
 //Middleware
 app.use(express.json());

c4da91c18b5361a3bb778d8896616fb80538d84d
unknown <ayt.ullah.dev@gmail.com>
2022-07-05 09:42:54 +0600

    JWT added in the user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const User = require('../models/userModels');
 // @desc Register a new user
@@ -32,7 +34,8 @@ const registerUser=asyncHandler(async (req,res)=>{
         res.status(201).json({
             _id:user._id,
             name:user.name,
-            email:user.email
+            email:user.email,
+            token:generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -52,7 +55,8 @@ const loginUser=asyncHandler(async (req,res)=>{
         res.status(200).json({
             _id:user._id,
             name:user.name,
-            emai:user.email
+            emai:user.email,
+            token:generateToken(user._id)
         })
     }else{
         res.status(401)
@@ -62,6 +66,10 @@ const loginUser=asyncHandler(async (req,res)=>{
    
 })
 
+// Generate Token
+const generateToken = (id) => {
+        return jwt.sign({id}, process.env.JWT_SECRET,{expiresIn:'30d'})
+}
 module.exports={
     registerUser,
     loginUser,
